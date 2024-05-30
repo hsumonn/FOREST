0403c10a41ad4fed464005a0fe93a81fb8657712
MAETEL\2230268 <jerryjohanes87@gmail.com>
2024-05-30 16:34:25 +0900

    update

--- lib/MainMenu.dart
+++ lib/MainMenu.dart
@@ -1,75 +1,122 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:location/location.dart';
 
 void main() {
-  runApp(const Umbrella());
+  runApp(const MyApp());
 }
 
-class Umbrella extends StatelessWidget {
-  const Umbrella({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Umbrella',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
-          useMaterial3: true,
-        ),
-      home: const HomePage(title: ''),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-
-  final String title;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> {
+  String _city = '';
+  double _temperature = 0;
+  String _iconUrl = '';
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
+  }
+
+  Future<void> _fetchWeather() async {
+    final location = Location();
+    final hasPermission = await location.hasPermission();
+    if (hasPermission == PermissionStatus.denied) {
+      await location.requestPermission();
+    }
+
+    final locData = await location.getLocation();
+    final apiKey = '003ef1d65597b85d2ab6fa19b59383b6'; // Replace with your OpenWeatherMap API key
+    final url =
+        'https://api.openweathermap.org/data/2.5/weather?lat=${locData.latitude}&lon=${locData.longitude}&units=metric&appid=$apiKey';
+
+    final response = await http.get(Uri.parse(url));
+    final data = json.decode(response.body);
+
     setState(() {
-      _counter++;
+      _city = data['name'];
+      _temperature = data['main']['temp'];
+      _iconUrl =
+      'http://openweathermap.org/img/wn/${data['weather'][0]['icon']}@2x.png';
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme
-            .of(context)
-            .colorScheme
-            .inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xFF9BE2F9), // Background color
+        ),
+        child: Stack(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    if (_city.isNotEmpty) ...[
+                      Image.network(_iconUrl),
+                      Text(
+                        '$_city, ${_temperature.toStringAsFixed(1)}°C',
+                        style: const TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _fetchWeather,
+                      child: const Text('Refresh Weather'),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .headlineMedium,
+            Positioned(
+              top: 20,
+              right: 20,
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.add),
+                    onPressed: () {
+                      // Handle add button press
+                    },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.info),
+                    onPressed: () {
+                      // Handle info button press
+                    },
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
