6ca3e4af46405f101b9fc595723a978fc698d6c1
hsumonn <hsumonn117@gmail.com>
2024-06-05 16:21:16 +0900

    詳細画面の作業の続き

--- lib/detailMenu.dart
+++ lib/detailMenu.dart
@@ -29,104 +29,108 @@ class DetailMenu extends StatelessWidget {
       'sunny.png'
     ];
 
-    return Center(
-      child: SizedBox(
-        width: 340, // Adjust as needed
-        height: 740, // Adjust as needed
-        child: Stack(
-          children: [
-            Align(
-              alignment: Alignment.topRight,
-              child: Row(
-                mainAxisSize: MainAxisSize.min, // Shrinks the Row to fit its children
-                children: [
-                  Image.asset(
-                    'images/registration.png', // First image asset path
-                    width: 40, // Adjust as needed
-                    height: 40, // Adjust as needed
-                    fit: BoxFit.cover, // Ensures the image covers the entire space allocated by its parent
-                  ),
-                  const SizedBox(width: 8), // Adds space between the images
-                  Image.asset(
-                    'images/Change.png', // Second image asset path
-                    width: 40, // Adjust as needed
-                    height: 40, // Adjust as needed
-                    fit: BoxFit.cover, // Ensures the image covers the entire space allocated by its parent
-                  ),
-                ],
-              ),
+    return SizedBox(
+      width: 340, // Adjust as needed
+      height: 740, // Adjust as needed
+
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topRight,
+            child: Row(
+              mainAxisSize: MainAxisSize.min, // Shrinks the Row to fit its children
+              children: [
+                Image.asset(
+                  'images/registration.png', // First image asset path
+                  width: 40, // Adjust as needed
+                  height: 40, // Adjust as needed
+                  fit: BoxFit.cover, // Ensures the image covers the entire space allocated by its parent
+                ),
+                const SizedBox(width: 8), // Adds space between the images
+                Image.asset(
+                  'images/Change.png', // Second image asset path
+                  width: 40, // Adjust as needed
+                  height: 40, // Adjust as needed
+                  fit: BoxFit.cover, // Ensures the image covers the entire space allocated by its parent
+                ),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 80), // Padding from the top
-              child: Column(
-                mainAxisSize: MainAxisSize.min, // Shrinks the Column to fit its children
-                children: [
-                  const Center(
-                    child: Text(
-                      '梅田', // Your text
-                      style: TextStyle(
-                        fontSize: 24, // Adjust as needed
-                        color: Colors.white, // Adjust as needed
-                      ),
-                    ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 80), // Padding from the top
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center, // Aligns children in the center horizontally
+              children: [
+                const Text(
+                  '梅田', // Your text
+                  style: TextStyle(
+                    fontSize: 24, // Adjust as needed
+                    color: Colors.white, // Adjust as needed
                   ),
-                  const SizedBox(height: 10), // Space between the text and the image
-                  Center(
-                    child: Image.asset(
-                      'images/heavy_rain.png', // Image asset path for the image under the text
-                      width: 100, // Adjust as needed
-                      height: 100, // Adjust as needed
-                      fit: BoxFit.cover, // Ensures the image covers the entire space allocated by its parent
-                    ),
+                ),
+                const SizedBox(height: 10), // Space between the text and the image
+                Image.asset(
+                  'images/heavy_rain.png', // Image asset path for the image under the text
+                  width: 100, // Adjust as needed
+                  height: 100, // Adjust as needed
+                  fit: BoxFit.cover, // Ensures the image covers the entire space allocated by its parent
+                ),
+                const SizedBox(height: 5), // Space between the image and the text
+                const Text(
+                  '45°C', // Text under the image
+                  style: TextStyle(
+                    fontSize: 24, // Adjust as needed
+                    color: Colors.white, // Adjust as needed
                   ),
-                  const SizedBox(height: 5), // Space between the image and the text
-                  const Center(
-                    child: Text(
-                      '45°C', // Text under the image
-                      style: TextStyle(
-                        fontSize: 24, // Adjust as needed
-                        color: Colors.white, // Adjust as needed
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 70), // Space before the new images
+                ),
+                const SizedBox(height: 70), // Space before the new images
 
-                  CustomPaint(
-                    size: const Size(double.infinity, 2), // Adjust height as needed
-                    painter: StraightLinePainter(),
-                  ),
-                  const SizedBox(height: 10), // Space between the line and the images
-                  SizedBox(
-                    height: 80,
-
+                CustomPaint(
+                  size: const Size(double.infinity, 2), // Adjust height as needed
+                  painter: StraightLinePainter(),
+                ),
+                const SizedBox(height: 10), // Space between the line and the images
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: SizedBox(
+                    height: 120, // Adjust height as needed
+                    // Adjust width as needed
                     child: PageView.builder(
-
                       scrollDirection: Axis.horizontal,
                       itemCount: imageNames.length, // Total number of images
-                      controller: PageController(viewportFraction: 1/4 ), // Show 4 images at a time without spacing
+                      controller: PageController(initialPage: 2, viewportFraction: 0.2), // Show 5 images at a time without spacing
                       itemBuilder: (context, index) {
-                        return Container(
-                          //width: MediaQuery.of(context).size.width / 4, // Ensuring 4 images are shown
-                          //margin: EdgeInsets.zero, // No margin between images
-                          child: Image.asset(
-
-                            'images/${imageNames[index]}', // Image asset path
-                            //fit: BoxFit.cover, // Ensures the image covers the entire space allocated by its parent
-                          ),
+                        return Column(
+                          children: [
+                            Image.asset(
+                              'images/${imageNames[index]}', // Image asset path
+                              width: 80, // Ensuring each image has a fixed width
+                              height: 80, // Ensuring each image has a fixed height
+                              fit: BoxFit.cover, // Ensures the image covers the entire space allocated by its parent
+                            ),
+                            const SizedBox(height: 12), // Space between the image and the text
+                            Text(
+                              'Text ${index + 1}', // Sample text under the image
+                              style: const TextStyle(
+                                fontSize: 12, // Adjust as needed
+                                color: Colors.white, // Adjust as needed
+                              ),
+                            ),
+                          ],
                         );
                       },
                     ),
                   ),
-                  const SizedBox(height: 10), // Space between the images and the line
-                  CustomPaint(
-                    size: const Size(double.infinity, 2), // Adjust height as needed
-                    painter: StraightLinePainter(),
-                  ),
-                ],
-              ),
+                ),
+                const SizedBox(height: 1), // Space between the images and the line
+                CustomPaint(
+                  size: const Size(double.infinity, 2), // Adjust height as needed
+                  painter: StraightLinePainter(),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
