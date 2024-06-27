d51c033439aaaa1a4bbf3a8ed595e42a5079dbc5
MSI\2230165 <2230165@ecc.ac.jp>
2024-06-27 11:26:35 +0900

    再確認画面のファイル作成

--- lib/confirm_menu.dart
+++ lib/confirm_menu.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() {
+
   WidgetsFlutterBinding.ensureInitialized();
 
   FlutterLocalNotificationsPlugin()
@@ -56,21 +60,48 @@ class _MyAppState extends State<MyHomePage> {
     );
   }
 
-  void _onPressed() {
-    showLocalNotification('今日のXX時に', '雨が降ります');                           //通知の内容を決めれる場所
-  }
+        Future<void> _onPressed() async {
+          const String apiKey = 'eed754aeda9ee52d698e40be18de7b9c';
+          const String apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=35.6895&lon=139.6917&appid=$apiKey';
+
+          final response = await http.get(Uri.parse(apiUrl));
+          if (response.statusCode == 200) {
+            final Map<String, dynamic> data = json.decode(response.body);
+            final String weatherDescription = data['weather'][0]['description'];
+            final bool isRainy = weatherDescription.toLowerCase().contains('rain');
+
+            showLocalNotification('今日のXX時に', '雨が降ります');                           //通知の内容を決めれる場所
+
+            if (isRainy) {
+              // 通知を表示する処理を実装
+              // 例: flutter_local_notificationsパッケージを使って通知を表示
+              showLocalNotification('今日のXX時に', '雨が降ります');                           //通知の内容を決めれる場所
+
+            }
+          } else {
+              if (kDebugMode) {
+                print('天気情報の取得に失敗しました。');
+              }
+            }
+        }
 
   void showLocalNotification(String title, String message) {
     const androidNotificationDetail = AndroidNotificationDetails(
-        'channel_id', // channel Id
-        'channel_name' // channel Name
+      'channel_id', // チャンネルID
+      'channel_name', // チャンネル名
+
+      importance: Importance.max,
+      priority: Priority.high,
+      color: Color.fromARGB(255, 0, 0, 0), // 色を指定
     );
+
+
     const iosNotificationDetail = DarwinNotificationDetails();
     const notificationDetails = NotificationDetails(
       iOS: iosNotificationDetail,
       android: androidNotificationDetail,
     );
-    FlutterLocalNotificationsPlugin()
-        .show(0, title, message, notificationDetails);
+    FlutterLocalNotificationsPlugin().show(0, title, message, notificationDetails);
   }
+
 }
